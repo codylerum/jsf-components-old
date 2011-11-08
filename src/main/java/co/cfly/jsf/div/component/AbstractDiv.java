ed2654b89411ad7c771bfaa00808383202e67373
Cody Lerum <cody.lerum@gmail.com>
2011-11-08 15:41:42 -0700

    package renames

--- src/main/java/co/cfly/jsf/div/component/AbstractDiv.java
+++ src/main/java/co/cfly/jsf/div/component/AbstractDiv.java
@@ -7,7 +7,7 @@ import org.richfaces.cdk.annotations.JsfRenderer;
 import org.richfaces.cdk.annotations.Tag;
  
 @JsfComponent(
-        type = "co.cfly.oss.jsf.components.div.div.Div", 
+        type = "co.cfly.jsf.div", 
         family = "co.cfly.layout", 
         renderer = @JsfRenderer(type = "co.cfly.oss.jsf.components.div"), 
         tag = @Tag(name="div"))
--- src/main/java/co/cfly/jsf/div/renderkit/DivRenderer.java
+++
@@ -1,7 +0,0 @@
-package co.cfly.jsf.div.renderkit;
-
-import org.richfaces.renderkit.RendererBase;
-
-public class DivRenderer extends RendererBase {
-
-}
