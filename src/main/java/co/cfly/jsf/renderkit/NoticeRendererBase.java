e1743f9895e860b040754d9d0af0b37ffbe6eddb
Cody Lerum <cody.lerum@gmail.com>
2011-11-09 11:28:09 -0700

    minor

--- src/main/java/co/cfly/jsf/renderkit/NoticeRendererBase.java
+++ src/main/java/co/cfly/jsf/renderkit/NoticeRendererBase.java
@@ -1,18 +1,23 @@
 package co.cfly.jsf.renderkit;
 
-
 import javax.faces.application.ResourceDependencies;
 import javax.faces.application.ResourceDependency;
+import javax.faces.component.UIComponent;
 
 import org.richfaces.renderkit.RendererBase;
+
+import co.cfly.jsf.core.Notices;
+
 /**
  * 
  * @author Cody Lerum
- *
+ * 
  */
-@ResourceDependencies({
-        @ResourceDependency(library = "javax.faces", name = "jsf.js"),
-        @ResourceDependency(name = "jquery.js")
-})
+@ResourceDependencies({ @ResourceDependency(library = "javax.faces", name = "jsf.js"), @ResourceDependency(name = "jquery.js") })
 public class NoticeRendererBase extends RendererBase {
+    
+    public Notices getNotices(UIComponent component)
+    {
+        return (Notices) component.getAttributes().get("notices");
+    }
 }
